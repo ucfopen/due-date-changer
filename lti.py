c3278937783d3c711777455986f9a5887db33e52
Matthew Emond <me@ucf.edu>
2016-11-15 18:05:25 -0500

    Removed table in favor of bootstrap grid layout. Created initial implementation of update_assignments. Currently only works on due dates, published flag, and lock/unlock dates.

--- lti.py
+++ lti.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+from datetime import datetime
+import re
+
 from flask import Flask, redirect, render_template, request, url_for, Response
 from pycanvas import Canvas
 from pylti.flask import lti
@@ -23,7 +27,7 @@ def error(exception=None):
 
 
 @app.route('/launch', methods=['POST', 'GET'])
-@lti(error=error, request='initial', role='any', app=app)
+@lti(error=error, request='initial', role='staff', app=app)
 def launch(lti=lti):
     course_id = request.form.get('custom_canvas_course_id')
 
@@ -37,17 +41,61 @@ def index(lti=lti):
 
 
 @app.route('/course/<course_id>/assignments', methods=['GET'])
-@lti(error=error, request='any', role='any', app=app)
+@lti(error=error, request='session', role='staff', app=app)
 def show_assignments(course_id, lti=lti):
     course = canvas.get_course(course_id)
     assignments = course.get_assignments()
-    for assignment in assignments:
-        print assignment.name, assignment.published
+
     return render_template(
         'assignments.htm.j2',
         assignments=assignments,
         course=course
     )
+
+
+@app.route('/course/<course_id>/update', methods=['POST'])
+@lti(error=error, request='session', role='staff', app=app)
+def update_assignments(course_id, lti=lti):
+    post_data = request.form
+
+    local_tz = timezone(config.TIME_ZONE)
+    assignment_field_map = defaultdict(dict)
+
+    for key, value in post_data.iteritems():
+        if not re.match(r'\d+-[a-z_]+', key):
+            continue
+
+        assignment_id, field_name = key.split('-')
+
+        if value is None:
+            value = ''
+        elif value == 'on':
+            value = True
+        else:
+            try:
+                value = datetime.strptime(value, config.LOCAL_TIME_FORMAT)
+                value = local_tz.localize(value)
+                value = value.isoformat()
+            except ValueError:
+                # Not a valid time. Just ignore.
+                value = ''
+
+        assignment_field_map[assignment_id].update({field_name: value})
+
+    course = canvas.get_course(course_id)
+    assignments = course.get_assignments()
+    for assignment in assignments:
+        fields = assignment_field_map.get(str(assignment.id))
+
+        # Unchecked checkboxes are not sent to the server, so need to manually
+        # set published to False if it isn't already set.
+        fields['published'] = fields.get('published', False)
+
+        if fields and isinstance(fields, dict):
+            assignment.edit(assignment=fields)
+
+    # temporarily redirecting back for debug purposes
+    return redirect(url_for('show_assignments', course_id=course_id))
 
 
 @app.route('/lti.xml', methods=['GET'])
@@ -59,7 +107,7 @@ def xml():
 
 
 @app.template_filter()
-def datetimeformat(utc_datetime, format='%m/%d/%Y %I:%M %p'):
+def utc_datetime_to_local_str(utc_datetime, format=config.LOCAL_TIME_FORMAT):
     if not utc_datetime.tzinfo:
         # Localize to UTC if there is no timezone information.
         utc_datetime = utc.localize(utc_datetime)
