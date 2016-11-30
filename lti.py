f5c68a69f53b871c2d385a12aa55694bcac35400
Matthew Emond <me@ucf.edu>
2016-11-30 12:25:42 -0500

    Added support for quiz hide/show dates. Added logging. Tweaked error page.

--- lti.py
+++ lti.py
@@ -1,9 +1,12 @@
 from collections import defaultdict
 from datetime import datetime
+import logging
+from logging.handlers import RotatingFileHandler
 import re
 
 from flask import Flask, redirect, render_template, request, url_for, Response
 from pycanvas import Canvas
+from pycanvas.exceptions import CanvasException
 from pylti.flask import lti
 from pytz import utc, timezone
 
@@ -13,15 +16,27 @@ import config
 app = Flask(__name__)
 app.config.from_object('config')
 
+formatter = logging.Formatter(config.LOG_FORMAT)
+handler = RotatingFileHandler(
+    config.LOG_FILE,
+    maxBytes=config.LOG_MAX_BYTES,
+    backupCount=config.LOG_BACKUP_COUNT
+)
+handler.setLevel(logging.getLevelName(config.LOG_LEVEL))
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
+
 canvas = Canvas(config.API_URL, config.API_KEY)
 
 
-@app.route('/error', methods=['GET'])
 def error(exception=None):
     return Response(
         render_template(
             'error.htm.j2',
-            messages=exception.get('exception', 'An unknown exception occured.')
+            message=exception.get(
+                'exception',
+                'Please contact your System Administrator.'
+            )
         )
     )
 
@@ -43,12 +58,31 @@ def index(lti=lti):
 @app.route('/course/<course_id>/assignments', methods=['GET'])
 @lti(error=error, request='session', role='staff', app=app)
 def show_assignments(course_id, lti=lti):
-    course = canvas.get_course(course_id)
-    assignments = course.get_assignments()
+    try:
+        course = canvas.get_course(course_id)
+        assignments = course.get_assignments()
+        quiz_dict = {quiz.id: quiz for quiz in course.get_quizzes()}
+    except CanvasException as err:
+        app.logger.exception('Error getting assignments from Canvas.')
+        return error({'exception': err})
+
+    assignment_quiz_list = []
+    for assignment in assignments:
+        if hasattr(assignment, 'quiz_id'):
+            quiz = quiz_dict.get(assignment.quiz_id)
+            if hasattr(quiz, 'show_correct_answers_at_date'):
+                assignment.show_correct_answers_at_date = utc_datetime_to_local_str(
+                    quiz.show_correct_answers_at_date
+                )
+            if hasattr(quiz, 'hide_correct_answers_at_date'):
+                assignment.hide_correct_answers_at_date = utc_datetime_to_local_str(
+                    quiz.hide_correct_answers_at_date
+                )
+        assignment_quiz_list.append(assignment)
 
     return render_template(
         'assignments.htm.j2',
-        assignments=assignments,
+        assignments=assignment_quiz_list,
         course=course
     )
 
@@ -56,6 +90,9 @@ def show_assignments(course_id, lti=lti):
 @app.route('/course/<course_id>/update', methods=['POST'])
 @lti(error=error, request='session', role='staff', app=app)
 def update_assignments(course_id, lti=lti):
+    # if not request.is_xhr:
+    #     pass  # ajax only?
+
     post_data = request.form
 
     local_tz = timezone(config.TIME_ZONE)
@@ -66,33 +103,58 @@ def update_assignments(course_id, lti=lti):
             continue
 
         assignment_id, field_name = key.split('-')
-
-        if value is None:
-            value = ''
-        elif value == 'on':
-            value = True
-        else:
-            try:
-                value = datetime.strptime(value, config.LOCAL_TIME_FORMAT)
-                value = local_tz.localize(value)
-                value = value.isoformat()
-            except ValueError:
-                # Not a valid time. Just ignore.
-                value = ''
-
         assignment_field_map[assignment_id].update({field_name: value})
 
-    course = canvas.get_course(course_id)
-    assignments = course.get_assignments()
-    for assignment in assignments:
-        fields = assignment_field_map.get(str(assignment.id))
+    def fix_date(value):
+        try:
+            value = datetime.strptime(value, config.LOCAL_TIME_FORMAT)
+            value = local_tz.localize(value)
+            return value.isoformat()
+        except ValueError:
+            # Not a valid time. Just ignore.
+            return ''
 
-        # Unchecked checkboxes are not sent to the server, so need to manually
-        # set published to False if it isn't already set.
-        fields['published'] = fields.get('published', False)
+    for assignment_id, field in assignment_field_map.iteritems():
+        assignment_type = field.get('assignment_type', 'assignment')
+        quiz_id = field.get('quiz_id')
+        try:
+            course = canvas.get_course(course_id)
+        except CanvasException:
+            app.logger.exception('Error getting course #{}.'.format(course_id))
 
-        if fields and isinstance(fields, dict):
-            assignment.edit(assignment=fields)
+        payload = {
+            'published': True if field.get('published') == 'on' else False,
+            'due_at': fix_date(field.get('due_at')),
+            'lock_at': fix_date(field.get('lock_at')),
+            'unlock_at': fix_date(field.get('unlock_at')),
+        }
+
+        if assignment_type == 'quiz' and quiz_id:
+            payload.update({
+                'show_correct_answers_at': fix_date(
+                    field.get('show_correct_answers_at')
+                ),
+                'hide_correct_answers_at': fix_date(
+                    field.get('hide_correct_answers_at')
+                )
+            })
+
+            try:
+                quiz = course.get_quiz(quiz_id)
+                quiz.edit(quiz=payload)
+            except CanvasException:
+                app.logger.exception('Error getting/editing quiz #{}.'.format(
+                    quiz_id
+                ))
+
+        else:
+            try:
+                assignment = course.get_assignment(assignment_id)
+                assignment.edit(assignment=payload)
+            except CanvasException:
+                app.logger.exception('Error getting/editing assignment #{}.'.format(
+                    assignment_id
+                ))
 
     # temporarily redirecting back for debug purposes
     return redirect(url_for('show_assignments', course_id=course_id))
